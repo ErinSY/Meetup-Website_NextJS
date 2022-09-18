a39dcdc0becab5d2d5fa5c1203052de9d92f7e93
seoyul <seoyul0203@gmail.com>
2022-09-18 11:38:07 +0900

    ADD: css Module.css 적용

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0a869a5ff5fe429f64cf";
+/******/ 	var hotCurrentHash = "a597d27756ca2f385186";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import MeetupDetail from '../../components/meetups/MeetupDetail';
 
 function Detail() {
-  return <div>Detail</div>;
+  return (
+    <MeetupDetail
+      image='https://images.unsplash.com/photo-1488646953014-85cb44e25828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1635&q=80'
+      title='Testpage'
+      address='Korea'
+      description='this is Korea'
+    />
+  );
 }
 
 export default Detail;
